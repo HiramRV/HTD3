37edb5d4e6174d3e78b80b579fbc97a4ddf0b812
HiramRV <rub15044@uvg.edu.gt>
2016-07-28 10:55:49 -0600

    Pre y posts

--- I_Sorts.java
+++ I_Sorts.java
@@ -1,36 +1,26 @@
  
 /**
- * @author carlos
- *
- */
+*I_CSorts, Esta interfaz define los metodos de ordenamiento
+*@version: 2.0
+*@author: 
+*@since 2016-07-28
+*/
 public interface I_Sorts<T> {
 	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
+	//pre: Recibe una lista ordenada o no ordenada 
+	//post: Retorna una lista ordenada
 	public T[] SelectionSort(T array[]);
 	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
+	//pre: Recibe una lista ordenada o no ordenada 
+	//post: Retorna una lista ordenada
 	public T[] InsertionSort(T array[]);
 	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
+	//pre: Recibe una lista ordenada o no ordenada 
+	//post: Retorna una lista ordenada
 	public T[] MergeSort(T array[]);
 	
-	/**
-	 * 
-	 * @param array
-	 * @return
-	 */
+	//pre: Recibe una lista ordenada o no ordenada 
+	//post: Retorna una lista ordenada
 	public T[] QuickSort(T array[]);
 	
 }

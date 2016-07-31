e8a89ee610ccb5824ddd2dabba428ab7c64a4799
HiramRV <rub15044@uvg.edu.gt>
2016-07-31 16:04:39 -0600

    MergeSort 3

--- Sorts.java
+++ Sorts.java
@@ -45,31 +45,29 @@ public class Sorts<T> implements I_Sorts<T> {
 	}
 
 	@Override
-	public T[] MergeSort(Comparable[ ] a, Comparable[ ] tmp, int left, int right, int rightEnd )
-    {
-        int leftEnd = right - 1;
-        int k = left;
-        int num = rightEnd - left + 1;
+public T[] MergeSort(T[] a, T[] tmp, int left, int right, int rightEnd) {
+		  int leftEnd = right - 1;
+	        int k = left;
+	        int num = rightEnd - left + 1;
 
-        while(left <= leftEnd && right <= rightEnd)
-            if(a[left].compareTo(a[right]) <= 0)
-                tmp[k++] = a[left++];
-            else
-                tmp[k++] = a[right++];
+	        while(left <= leftEnd && right <= rightEnd)
+	            if(a[left].compareTo\(a[right]) <= 0)
+	                tmp[k++] = a[left++];
+	            else
+	                tmp[k++] = a[right++];
 
-        while(left <= leftEnd)    // Copy rest of first half
-            tmp[k++] = a[left++];
+	        while(left <= leftEnd)    // Copy rest of first half
+	            tmp[k++] = a[left++];
 
-        while(right <= rightEnd)  // Copy rest of right half
-            tmp[k++] = a[right++];
+	        while(right <= rightEnd)  // Copy rest of right half
+	            tmp[k++] = a[right++];
 
-        // Copy tmp back
-        for(int i = 0; i < num; i++, rightEnd--){
-            a[rightEnd] = tmp[rightEnd];
-        }
-            
-        return a
-    }
+	        // Copy tmp back
+	        for(int i = 0; i < num; i++, rightEnd--){
+	            a[rightEnd] = tmp[rightEnd];
+	        }
+	            
+	        return (T[]) a;
 
 	@Override
 	public T[] QuickSort(T[] array) {

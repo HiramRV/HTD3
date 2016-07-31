79472140061ffc9e7501f92b31339d20ea489dd2
HiramRV <rub15044@uvg.edu.gt>
2016-07-31 11:13:00 -0600

    MergeSort

--- Sorts.java
+++ Sorts.java
@@ -27,10 +27,28 @@ public class Sorts<T> implements I_Sorts<T> {
 	}
 
 	@Override
-	public T[] MergeSort(T[] array) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public T[] MergeSort(Comparable[ ] a, Comparable[ ] tmp, int left, int right, int rightEnd )
+    {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while(left <= leftEnd && right <= rightEnd)
+            if(a[left].compareTo(a[right]) <= 0)
+                tmp[k++] = a[left++];
+            else
+                tmp[k++] = a[right++];
+
+        while(left <= leftEnd)    // Copy rest of first half
+            tmp[k++] = a[left++];
+
+        while(right <= rightEnd)  // Copy rest of right half
+            tmp[k++] = a[right++];
+
+        // Copy tmp back
+        for(int i = 0; i < num; i++, rightEnd--)
+            a[rightEnd] = tmp[rightEnd];
+    }
 
 	@Override
 	public T[] QuickSort(T[] array) {

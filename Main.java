75521f721f2cdf188fef7a30750f30575926d2f0
HiramRV <rub15044@uvg.edu.gt>
2016-08-01 22:16:45 -0600

    Encabezado

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 /**
- * @author carlos
- *
- */
+*I_Sorts, Esta interfaz define los metodos de ordenamiento
+*@version: 2.0
+*@author: Luis Sierra, 131074 // Carlos Solorzano, 08832 // Steven Rubio, 15044 // Diego Lopez, 141222
+*@since 2016-08-1
+*/
+
 import static java.lang.System.*;
 import java.util.Scanner;
 public class Main {

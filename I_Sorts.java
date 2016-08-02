ef94a23da8f8383581336e2e593ce135b9962ce1
HiramRV <rub15044@uvg.edu.gt>
2016-08-01 22:16:03 -0600

    Encabezado

--- I_Sorts.java
+++ I_Sorts.java
@@ -1,8 +1,10 @@
  
 /**
- * @author carlos
- *
- */
+*I_Sorts, Esta interfaz define los metodos de ordenamiento
+*@version: 2.0
+*@author: Luis Sierra, 131074 // Carlos Solorzano, 08832 // Steven Rubio, 15044 // Diego Lopez, 141222
+*@since 2016-01-08
+*/
 public interface I_Sorts<T> {
 	
 	/**
